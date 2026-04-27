289497d26801ecaa5f5e80b4d011f5e604ef9670
krishnaboini <55269233+krishnaboini@users.noreply.github.com>
2026-04-27 22:52:31 +0530

    Update index.js
    refactor: rename function for clarity

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { ethers } = require("ethers");
 
 const provider = new ethers.JsonRpcProvider("https://mainnet.base.org");
 console.log("Connecting to Base network...");
-async function checkBalance() {
+async function getWalletBalance()
   const balance = await provider.getBalance("0x0000000000000000000000000000000000000000");
   console.log("Balance:", ethers.formatEther(balance));
 }
 
-checkBalance();
+getWalletBalance();
