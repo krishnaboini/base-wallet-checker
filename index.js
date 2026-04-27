5c20e6d7d3f267b0b300b32e1bbbc511edadcfe6
krishnaboini <55269233+krishnaboini@users.noreply.github.com>
2026-04-27 22:51:14 +0530

    Update index.js
    feat: add connection log message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const { ethers } = require("ethers");
 
 const provider = new ethers.JsonRpcProvider("https://mainnet.base.org");
-
+console.log("Connecting to Base network...");
 async function checkBalance() {
   const balance = await provider.getBalance("0x0000000000000000000000000000000000000000");
   console.log("Balance:", ethers.formatEther(balance));
